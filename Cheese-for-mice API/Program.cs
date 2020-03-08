0de8f89b271db9ae6d2e10101d8b4a3508b4adb9
Shanks97 <juanp_cm97@hotmail.com>
2020-03-08 12:22:45 -0500

    Agregar archivos de proyecto.

---
+++ Cheese-for-mice API/Program.cs
@@ -0,0 +1,32 @@
+﻿using API_Library.Manager;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cheese_for_mice_API
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("starting");
+            try
+            {
+                //Mouse
+                var user = "Tocutoeltuco";                
+                var mouse = await MouseManager.GetMouse(user);
+
+                //Tribe
+                var tribe = await TribeManager.GetTribe("Runtime error");
+                var tribe_1 = await TribeManager.GetTribeByUser(user);
+                var tribe_2 = await TribeManager.SearchTribe("Runtime e");
+                
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
